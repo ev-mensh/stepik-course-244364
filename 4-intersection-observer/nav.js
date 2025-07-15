1f9b32896b2e3186c2763995d618bfefdcb3cbde
Evgenii Menshikov <ev.mensh@gmail.com>
2025-07-16 00:15:28 +0200

    4 intersection observer: Init

---
+++ 4-intersection-observer/nav.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let nav = document.querySelector(".nav");
+  document.fonts.ready.then(() => {
+    initNav(nav);
+  });
+});
+
+function initNav(nav) {
+  let links = [...nav.querySelectorAll("a")];
+  let sections = links.map((link) =>
+    document.querySelector(link.getAttribute("href"))
+  );
+
+  // scroll to the relevant section when clicking on the link in the navigation
+  links.forEach((link, index) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      let sectionY = sections[index].getBoundingClientRect().y;
+      let navHeight = nav.getBoundingClientRect().height;
+
+      window.scrollTo({
+        behavior: "smooth",
+        top: window.scrollY + sectionY - navHeight,
+      });
+    });
+  });
+
+  // highlight an active navigation link
+  // >>>>>> START HERE <<<<<<
+}
