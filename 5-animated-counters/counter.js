ab28514e08d0360d882fc60b6b1b504047f2b19e
Evgenii Menshikov <ev.mensh@gmail.com>
2025-08-27 11:28:47 +0200

    5 animated counters: solve

--- 5-animated-counters/counter.js
+++ 5-animated-counters/counter.js
@@ -1,1 +1,32 @@
-// Start here
+const ANIMATION_TIME = 1000;
+
+let start;
+let elements;
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.fonts.ready.then(() => {
+    initCounters();
+  });
+});
+
+function initCounters() {
+  elements = document.querySelectorAll(".counter");
+  requestAnimationFrame(step);
+}
+
+function step(timestamp) {
+  if (start === undefined) {
+    start = timestamp;
+  }
+  let elapsed = timestamp - start;
+
+  elements.forEach((item) => {
+    let max = parseInt(item.dataset.max);
+    let nextValue = Math.min((elapsed * max) / ANIMATION_TIME, max).toFixed(0);
+    item.innerText = nextValue;
+  });
+
+  if (elapsed < ANIMATION_TIME) {
+    requestAnimationFrame(step);
+  }
+}
